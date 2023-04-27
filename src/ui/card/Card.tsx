2c2d62b2835381fae583f975fa258e84f3cfa9f1
Aleksandra Kuczmierczyk <kuczmierczyk.al@gmail.com>
2023-04-28 00:00:24 +0200

    chore: delete useless component

--- src/ui/card/Card.tsx
+++
@@ -1,6 +0,0 @@
-import { Paper } from "@mui/material";
-import { CardProps } from "./Card.types";
-
-export const Card = ({ children, sx }: CardProps) => {
-  return <Paper sx={sx}>{children}</Paper>;
-};
--- src/ui/card/Card.types.ts
+++
@@ -1,6 +0,0 @@
-import { SxProps } from "@mui/material";
-
-export interface CardProps {
-  children: React.ReactNode;
-  sx?: SxProps;
-}
