66ef229af0dbdd6c48f1a9812696c0a790f9ced1
Aleksandra Kuczmierczyk <kuczmierczyk.al@gmail.com>
2023-05-29 18:57:54 +0200

    chore: add Tab Menu component

---
+++ src/components/tab-menu/TabMenu.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { Paper } from "@mui/material";
+
+export const TabMenu = ({ variant }: { variant: keyof typeof tabs }) => {
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  const tabs = {
+    explorePage: ["Top", "Latest", "People", "Media"],
+    bookmarksPage: ["Tweets", "Tweets & replies", "Media", "Likes"],
+  };
+
+  return (
+    <Paper sx={{ py: 3 }}>
+      <Tabs
+        orientation="vertical"
+        onChange={handleChange}
+        value={value}
+        TabIndicatorProps={{ hidden: true }}
+        sx={{
+          "& button": {
+            alignItems: "flex-start",
+            minHeight: 32,
+            px: 2,
+            py: 0.5,
+            my: 0.5,
+            borderLeft: "3px solid transparent",
+          },
+          "& button.Mui-selected": {
+            borderColor: "primary.main",
+            transition: " 0.5s",
+          },
+        }}
+      >
+        {tabs[variant].map((tab) => (
+          <Tab disableRipple label={tab} />
+        ))}
+      </Tabs>
+    </Paper>
+  );
+};
